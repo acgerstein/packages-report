321c1182312e8e2e13508e89a677483dcb5aa563
Aleeza Gerstein <aleeza.gerstein@umanitoba.ca>
2019-01-15 17:01:48 -0600

    Add YAML

--- test.R
+++ test.R
@@ -1,6 +1,9 @@
+library(tidyverse)
+
 .libPaths()
 
-ip <- installed.packages() %>%
-  as.tibble() %>%
-  select("Package", "LibPath", "Version", "Priority", "Built") %>%
-  readr::write_csv(here::here("data/installed-packages.csv"))
+ip <- installed.packages() #%>%
+#  as.tibble() %>%
+#  select("Package", "LibPath", "Version", "Priority", "Built") %>%
+  
+nrow(ip)
